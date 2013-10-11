e1f3d9878fbb027a9122ff2fc7c2825558c6bbc8
YukinoIkegami <yukino0131@me.com>
2013-10-12 01:19:18 +0900

    Move C++ files to lib/

--- setup.py
+++ setup.py
@@ -3,14 +3,18 @@
 from distutils.core import setup, Extension
 
 
-example_module = Extension('_oll',
-                           sources=['oll.cpp','oll_swig_wrap.cxx'],
+oll_module = Extension('_oll',
+                           sources=['lib/oll.cpp','oll_swig_wrap.cxx'],
+                           include_dirs=['lib'],
+                           language='c++'
                            )
 
 setup (name = 'oll',
        version = '0.0.1',
        author      = "Yukino Ikegami",
        description = """OLL for Python""",
-       ext_modules = [example_module],
+       ext_modules = [oll_module],
        py_modules = ["oll"],
        )
+
+
