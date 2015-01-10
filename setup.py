c002aa6bbd80ff657824991f9c4cb5ac378698ed
Yukino Ikegami <yukino0131@me.com>
2015-01-11 06:28:28 +0900

    Add Python 3.4

--- setup.py
+++ setup.py
@@ -33,11 +33,10 @@ setup(
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
         'Topic :: Scientific/Engineering :: Information Analysis',
         'Topic :: Text Processing :: Linguistic'
