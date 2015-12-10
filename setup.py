39e0c6b524a0f4d28db0725321aee998bf5f25f8
Yukino Ikegami <yukino0131@me.com>
2015-12-11 07:41:07 +0900

    Python 2.6 is no longer supported

--- setup.py
+++ setup.py
@@ -33,7 +33,6 @@ setup(
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
